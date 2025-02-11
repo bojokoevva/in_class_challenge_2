ed94511841eba508329d84c42d7700f51e9f635a
bojokoeva <bojokoeva34@gmail.com>
2025-02-11 16:27:30 -0500

    Completed Task 2

--- cc_example.js
+++ cc_example.js
@@ -12,6 +12,21 @@ function getProductsByCategory(products_array, category_name) {
 console.log(getProductsByCategory(products, "Electronics"));
 
 
+// Task 2
 
 
+console.log("*". repeat(20))
+function applyDiscount(products, discountRate) {
+    return products.map(product => ({
+        ...product,
+        price: product.price - (product.price * discountRate)
+    }
+
+    ))
+}
+
+
+console.log(applyDiscount(products, .1))
+console.log("*". repeat(20))
+console.log(products)
 
