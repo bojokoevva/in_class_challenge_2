e4e644bfc3d405b5eacde116db05d3b55e34c2d2
bojokoeva <bojokoeva34@gmail.com>
2025-02-11 16:06:22 -0500

    Complet Task 1

--- cc_example.js
+++ cc_example.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+// Task 1
+
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Phone", price: 800, category: "Electronics"}
+];
+
+function getProductsByCategory(products_array, category_name) { 
+    return products_array.filter(product => product.category === category_name);
+}
+
+console.log(getProductsByCategory(products, "Electronics"));
+
+
+
+
+
